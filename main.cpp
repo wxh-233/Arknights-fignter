f4537ad087f5197fed7c8493cf1f33d094ba029e
Wang Xiaohang <93416090+wxh-233@users.noreply.github.com>
2023-03-11 13:40:09 +0800

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<Windows.h>
+#include"tool/tool.h"
+#include"tool/game.h"
+#include"cards/Arkcards.h"
+using namespace std;
+using namespace tool;
+using namespace game;
+using namespace Arkcards;
+int main()
+{
+
+	return 0;
+}
