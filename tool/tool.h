1d19b5aa40b558534e95ac601027a414e8976d7d
Wang Xiaohang <93416090+wxh-233@users.noreply.github.com>
2023-03-13 16:29:39 +0800

    Create tool.h

--- tool/tool.h
+++ tool/tool.h
@@ -1,6 +1,12 @@
 #pragma once
 #include<bits/stdc++.h>
 #include<Windows.h>
+#define cls system("cls")
+#define pause system("pause")
+#define S Sleep(1000)
+#define ss Sleep(200)
+#define s Sleep(50)
+
 using namespace std;
 namespace tool {
 
