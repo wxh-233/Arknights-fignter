5f14d1c8150486eb2598a2f43aed93ce0ff2983d
Wang Xiaohang <93416090+wxh-233@users.noreply.github.com>
2023-03-14 16:08:47 +0800

    Update game.h

--- tool/game.h
+++ tool/game.h
@@ -2,7 +2,40 @@
 #include<bits/stdc++.h>
 #include<Windows.h>
 #include"tool.h"
+#include"..\cards\enemy.h"
+#include"..\cards\Arkcards.h"
 using namespace std;
-namespace game {
+using namespace tool;
+uging namespace enemy;
+using namespace Arkcards;
 
+namespace game {
+	int dt[50];
+	struct player_bz {
+		int Hp = 100;
+		int kip = 10;
+		int wf = 5;
+		int fw = 50;
+	};
+	void PVP()
+	{
+
+	}
+	void PVE()
+	{
+		//速度
+		int e_num=rand()/2+1;
+		struct somebody{
+			int Hp;
+			int kip;
+			int wf;
+			int fw;
+		}p[5];
+		player_bz bz;
+		p[1]=bz
+		for (int i = 1; i <= e_num; i++)
+		{
+			
+		}
+	}
 }
