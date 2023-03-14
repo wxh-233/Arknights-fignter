4652bd963e538edb2c2cc2d1a03f7ca360963b1f
Wang Xiaohang <93416090+wxh-233@users.noreply.github.com>
2023-03-14 16:08:17 +0800

    Update Arkcards.h

--- cards/Arkcards.h
+++ cards/Arkcards.h
@@ -4,5 +4,14 @@
 #include"..\tool\tool.h"
 using namespace std;
 namespace Arkcards {
-
+	struct {
+		string talk;
+		int cost_use;
+		int kip;
+		int kip_rush;
+		int run;
+	};
+	void start_make_Arkcards()
+	{
+	}
 }
