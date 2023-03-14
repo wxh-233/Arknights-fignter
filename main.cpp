882591a5bdb2a0b2c9cbf1e2797d247b0c106d5f
Wang Xiaohang <93416090+wxh-233@users.noreply.github.com>
2023-03-14 12:19:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,45 @@ using namespace std;
 using namespace tool;
 using namespace game;
 using namespace Arkcards;
+void chose_gamemode()
+{
+	cls;
+	int chose=0;
+	printf("1.PVP\n");
+	printf("2.PVE\n");
+	cin >> chose;
+	if (chose == 1)
+	{
+		chose1;
+		int player_number=0;
+		printf("请输入玩家数\n");
+		cin >> player_number;
+		if (player_number < 2)
+		{
+			printf("???\n")
+			S;
+			goto chose1;
+		}
+		/*
+		cls;
+		string names;
+		for(int i=1;i<=player_number;i++)
+		{
+			printf("请输入玩家%d名字,&names)
+			search()
+		}
+		//找存档
+		*/
+		play_start(player_number);
+	}
+	if (chose == 2)
+	{
+
+	}
+	chose_gamemode();
+}
 int main()
 {
-
+	chose_gamemode();
 	return 0;
 }
